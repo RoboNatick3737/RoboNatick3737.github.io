1884de8d54b227a1ea705a3fce1b70078caebd18
Alik (School Laptop) <Albert@Alberts-Air.fios-router.home>
2017-02-06 23:26:41 -0500

    Sped up scrolling

--- js/scrolling.js
+++ js/scrolling.js
@@ -6,7 +6,7 @@ $(function() {
         if (target.length) {
             $('html,body').animate({
                 scrollTop: target.offset().top
-            }, 1000);
+            }, 600);
         return false;
         }
         }
